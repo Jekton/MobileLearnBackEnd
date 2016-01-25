4eb811e611f360019519be6a901da75d7934f9b0
Jekton <ljtong64@outlook.com>
2016-01-25 15:24:53 +0800

    Add a db.js to handle db connection and init schema and model

---
+++ server/model/db.js
@@ -0,0 +1,53 @@
+'use strict';
+
+let mongoose = require('mongoose');
+const DB_URL = 'mongodb://localhost/mobilelearn';
+
+if (process.env.NODE_ENV === 'production') {
+    DB_URL = process.env.MONGOLAB_URI;
+}
+
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+function gracefulShutdown(msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+
+mongoose.connect(DB_URL);
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to ' + DB_URL);
+});
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected');
+});
+
+// For nodemon restarts
+process.once('SIGUSR2', function () {
+    gracefulShutdown('nodemon restart', function () {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+// For app termination
+process.on('SIGINT', function () {
+    gracefulShutdown('app termination', function () {
+        process.exit(0);
+    });
+});
+// For Heroku app termination
+/*process.on('SIGTERM', function () {
+    gracefulShutdown('Heroku app termination', function () {
+        process.exit(0);
+    });
+});*/
+
+
+// BRING IN YOUR SCHEMAS & MODELS
+require('./user');
