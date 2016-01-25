12fa0a916c8d48f33324f39995f0de09b05de43f
Jekton <ljtong64@outlook.com>
2016-01-25 16:07:25 +0800

    Add some constant to represent Capabilities

---
+++ server/model/capability.js
@@ -0,0 +1,31 @@
+module.exports.UserCapability = {
+    // prevent unintentional modification
+    get CAP_ADMIN() {
+        return 1;
+    },
+    get CAP_CREATE_COURSE() {
+        return 2;
+    },
+    get CAP_UPLOAD_LECTURE() {
+        return 4;
+    },
+    get CAP_UPLOAD_FILE() {
+        return 8;
+    },
+    get CAP_ADD_REVIEW() {
+        return 16;
+    }
+};
+
+
+module.exports.CourseCapability = {
+    get CAP_OWN() {
+        return 1;
+    },
+    get CAP_ADMIN() {
+        return 2;
+    },
+    get CAP_TAKING() {
+        return 4;
+    }
+};
